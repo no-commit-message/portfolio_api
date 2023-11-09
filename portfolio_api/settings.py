4a2e22b2a6d704529d173184cb9effe8aa43852e
川原歩夢 <ayumu@kawaharumenoAir.AirPort>
2023-11-09 23:11:10 +0900

    Update ALLOWED_HOSTS

--- portfolio_api/settings.py
+++ portfolio_api/settings.py
@@ -21,7 +21,10 @@ SECRET_KEY = env("SECRET_KEY")
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = env("DEBUG")
 
-ALLOWED_HOSTS = ["*"]
+ALLOWED_HOSTS = [
+    "portfolio-api-drf-d6a306e8536f.herokuapp.com/",
+    "localhost",
+]
 
 
 # Application definition

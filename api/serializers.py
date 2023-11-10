54e20c0e7dd11963ff72a78a0468908410011ff0
川原歩夢 <ayumu@kawaharumenoAir.AirPort>
2023-11-10 10:09:27 +0900

    Update serializers.py to change order and format

--- api/serializers.py
+++ api/serializers.py
@@ -2,9 +2,9 @@ from rest_framework import serializers
 from .models import Post
 
 class PostSerializer(serializers.ModelSerializer):
-    created_at = serializers.DateTimeField(format="%Y-%m-%d %H:%M:%s", read_only=True)
-    updated_at = serializers.DateTimeField(format="%Y-%m-%d %H:%M:%s", read_only=True)
+    created_at = serializers.DateTimeField(format="%Y-%m-%d %H:%M", read_only=True)
+    updated_at = serializers.DateTimeField(format="%Y-%m-%d %H:%M", read_only=True)
 
     class Meta:
         model = Post
-        fields = "__all__"
+        fields = ("id", "title", "content", "skill_level", "created_at", "updated_at")
